89ab5665a5915b0b61f51786baed12d194f36edc
RIO-bn <rionaldho1773@gmail.com>
2025-10-07 06:23:42 +0700

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyNotes.Models;
+
+namespace MyNotes.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MyNotes.Models.Note> Note { get; set; } = default!;
+    }
+}
